6b8dc411b88a4323c571fa501b1334a2da24732b
Christian <christian.dehek@gmail.com>
2021-01-08 15:37:47 -0700

    finished up reaction component, conditional rendering

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/react-hooks';
-import { QUERY_THOUGHT } from '../utils/queries'; 
-import ReactionList from '../components/ReactionList';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
+import { QUERY_THOUGHT } from "../utils/queries";
+import ReactionList from "../components/ReactionList";
+import Auth from '../utils/auth';
+import ReactionForm from '../components/ReactionForm';
 
-const SingleThought = props => {
-
+const SingleThought = (props) => {
   const { id: thoughtId } = useParams();
   console.log(thoughtId);
 
   const { loading, data } = useQuery(QUERY_THOUGHT, {
-    variables: { id: thoughtId }
+    variables: { id: thoughtId },
   });
 
   const thought = data?.thought || {};
@@ -25,7 +26,7 @@ const SingleThought = props => {
         <p className="card-header">
           <span style={{ fontWeight: 700 }} className="text-light">
             {thought.username}
-          </span>{' '}
+          </span>{" "}
           thought on {thought.createdAt}
         </p>
         <div className="card-body">
@@ -33,7 +34,10 @@ const SingleThought = props => {
         </div>
       </div>
 
-      {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+      {thought.reactionCount > 0 && (
+        <ReactionList reactions={thought.reactions} />
+      )}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
